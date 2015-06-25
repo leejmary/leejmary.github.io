9ae002267ed4b1a6d4048996b3147c23e767361e
leejmary <Mary@Marys-MacBook-Pro.local>
2015-06-25 15:21:48 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('li').click(function () {
+	var dataTabValue = $(this).data('tab');
+	$(this).addClass('selected').siblings().removeClass('selected');
+
+	$('.content-area:visible').fadeOut(function () {
+		$('.content-area-' + dataTabValue).fadeIn('slow');
+	});
+});
