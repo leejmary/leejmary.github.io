c1d4c56cc20269994b93850aa752e2856bdaf5b8
leejmary <Mary@Marys-MacBook-Pro.local>
2015-06-26 18:07:36 -0500

    Updated stylesheet and coming soon copy.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-$('li').click(function () {
-	var dataTabValue = $(this).data('tab');
-	$(this).addClass('selected').siblings().removeClass('selected');
 
-	$('.content-area:visible').fadeOut(function () {
-		$('.content-area-' + dataTabValue).fadeIn('slow');
+$(function(){
+	$('li').click(function () {
+		var dataTabValue = $(this).data('tab');
+		$(this).addClass('selected').siblings().removeClass('selected');
+
+		$('.content-area:visible').fadeOut(function () {
+			$('.content-area-' + dataTabValue).fadeIn('slow');
+		});
+
 	});
+
+  	function testAnim(x) {
+    $('.fa-heart').removeClass(x + " animated").addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass(x);
+    	});
+  	}
+
+	$(".fa-heart").on("mouseenter", function(e){
+		e.preventDefault();
+		testAnim("wobble");
+	});
+
 });
